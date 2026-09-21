25b3054ffd314800da31b8fa3f0f3c2e5c189002
jukomi <lancearquillano@gmail.com>
2026-09-21 17:09:45 +0800

    Add overtime pay for shifts over 8 hours - arquillano.lancebenedict

--- shifts.js
+++ shifts.js
@@ -3,6 +3,12 @@ function isValidShift(hours) {
 }
 
 function calculatePay(hours, rate) {
+  if (hours > 8){
+    const regularPay = REGULAR_HOURS_LIMIT * rate;
+    const overtimeHours = hours - REGULAR_HOURS_LIMIT;
+    const overtimePay = overtimeHours * (rate * 1.5);
+    return Math.floor(regularPay + overtimePay);
+  }
   return Math.floor(hours * rate);
 }
 
